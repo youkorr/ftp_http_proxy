e7ee45839a7de87b55cde347705b292b07fb550c
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-28 00:01:15 +0100

    Update __init__.py

--- components/ftp_http_proxy/__init__.py
+++ components/ftp_http_proxy/__init__.py
@@ -1,3 +1,7 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import media_player
+
 CONF_SERVER = 'server'
 CONF_USERNAME = 'username'
 CONF_PASSWORD = 'password'
