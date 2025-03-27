c3252cc4e18ec9c4d1a7eb72937ddf02c4ab1a97
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-27 23:26:27 +0100

    Create __init__.py

---
+++ components/ftp_http_proxy/__init__.py
@@ -0,0 +1,28 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import media_player
+
+DEPENDENCIES = ['media_player']
+AUTO_LOAD = ['media_player']
+
+ftp_http_proxy_ns = cg.esphome_ns.namespace('ftp_http_proxy')
+FTPHTTPProxy = ftp_http_proxy_ns.class_('FTPHTTPProxy', cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.Required('server'): cv.string,
+    cv.Required('username'): cv.string,
+    cv.Required('password'): cv.string,
+    cv.Optional('remote_path', default='/'): cv.string,
+    cv.Optional('local_port', default=8000): cv.port,
+})
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    
+    # Configuration des paramètres
+    cg.add(var.set_ftp_server(config['server']))
+    cg.add(var.set_username(config['username']))
+    cg.add(var.set_password(config['password']))
+    cg.add(var.set_remote_path(config['remote_path']))
+    cg.add(var.set_local_port(config['local_port']))
