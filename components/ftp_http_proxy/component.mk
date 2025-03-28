c206bde928e1459a10fd4cfcacaab59294b13fc4
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-28 06:01:14 +0100

    Update component.mk

--- components/ftp_http_proxy/component.mk
+++ components/ftp_http_proxy/component.mk
@@ -1,3 +1,5 @@
 # Configuration pour ESP-IDF
 COMPONENT_ADD_INCLUDEDIRS := .
 COMPONENT_PRIV_REQUIRED := esp_http_server lwip
+COMPONENT_ADD_INCLUDEDIRS := .
+COMPONENT_PRIV_REQUIRES := lwip esp_http_server esp_netif
