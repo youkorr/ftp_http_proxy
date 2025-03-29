8e6d3376e50b687778638951b64c469528c80cb3
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-29 01:26:22 +0100

    Update ftp_http_proxy.h

--- components/ftp_http_proxy/ftp_http_proxy.h
+++ components/ftp_http_proxy/ftp_http_proxy.h
@@ -1,11 +1,11 @@
 #pragma once
 
-#include "esphome/components/component.h"
-#include "esphome/core/component.h"
-#include "esphome/core/helpers.h"
+#include "esphome.h"
 #include <WiFiClient.h>
 #include <WebServer.h>
 #include <ESP32FtpServer.h>
+#include <vector>
+#include <string>
 
 namespace esphome {
 namespace ftp_http_proxy {
@@ -14,11 +14,12 @@ class FTPHTTPProxy : public Component {
  public:
   void setup() override;
   void loop() override;
+  float get_setup_priority() const override { return esphome::setup_priority::AFTER_WIFI; }
   
-  void set_ftp_server(const std::string &amp;server) { ftp_server_ = server; }
-  void set_username(const std::string &amp;username) { username_ = username; }
-  void set_password(const std::string &amp;password) { password_ = password; }
-  void add_remote_path(const std::string &amp;path) { remote_paths_.push_back(path); }
+  void set_ftp_server(const std::string &server) { ftp_server_ = server; }
+  void set_username(const std::string &username) { username_ = username; }
+  void set_password(const std::string &password) { password_ = password; }
+  void add_remote_path(const std::string &path) { remote_paths_.push_back(path); }
   void set_local_port(uint16_t port) { local_port_ = port; }
 
  protected:
@@ -36,7 +37,7 @@ class FTPHTTPProxy : public Component {
   void handle_file_();
   void setup_ftp_();
   void setup_http_();
-  bool fetch_file_(const std::string &amp;path, std::string &amp;content);
+  bool fetch_file_(const std::string &path, std::string &content);
 };
 
 }  // namespace ftp_http_proxy
